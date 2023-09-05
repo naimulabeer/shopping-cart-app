6f1861a0410d7b78c454ad3ec2705883c461c70a
naimulabeer <naimul.h.abeer@gmail.com>
2023-09-05 06:19:53 +0600

    fixed some issues in cart dropdown

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CartProvider } from "./contexts/cart-context.jsx";
 import { ProductsProvider } from "./contexts/product-context.jsx";
 
 import App from "./App.jsx";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
